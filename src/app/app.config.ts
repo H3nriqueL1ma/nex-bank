82a0f454f4234dc1cb29c252a3c236e60bcf0052
H3nriqueL1ma <henriquecarlos361893@gmail.com>
2024-08-19 16:33:31 -0300

    feat(canvas/modal cookies): Adição das funcionalidades do canvas dos cookies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {CookieService} from "ngx-cookie-service";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), CookieService]
 };
--- src/app/components/miscellaneous-components/modals-and-canvas/canvas-cookies/canvas-cookies.component.ts
+++ src/app/components/miscellaneous-components/modals-and-canvas/canvas-cookies/canvas-cookies.component.ts
@@ -1,34 +1,96 @@
 import {AfterViewInit, Component} from '@angular/core';
 import {NgIf} from "@angular/common";
+import {RouterLink, RouterLinkActive} from "@angular/router";
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-canvas-cookies',
   standalone: true,
   imports: [
-    NgIf
+    NgIf,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './canvas-cookies.component.html',
   styleUrl: './canvas-cookies.component.scss'
 })
 export class CanvasCookiesComponent implements AfterViewInit {
+  private canvasElement: HTMLElement = <HTMLElement>document.querySelector("#canvas");
+  private buttonCloseCookies: HTMLElement = <HTMLElement>document.querySelector("#close-cookies");
+  private modalElement: HTMLElement = <HTMLElement>document.querySelector("#modal-cookies");
+  private buttonCloseSettings: HTMLElement = <HTMLElement>document.querySelector("#close-settings");
+
+  private preferences: { [key: string]: boolean } = {
+    necessary: true,
+    functionality: false
+  }
+
+  constructor(private cookieService: CookieService) {
+  }
+
+  acceptCookies(): void {
+    this.cookieService.set("cookieConsent", "accepted", 365);
+    this.hideCanvasCookies();
+  }
+
+  rejectCookies(): void {
+    this.cookieService.set("cookiesConsent", "rejected", 365);
+    this.hideCanvasCookies();
+
+    for (let key in this.preferences) {
+      if (key !== "necessary") {
+        this.cookieService.delete(key);
+      }
+    }
+  }
+
+  saveCookiesSettings(): void {
+    for (let key in this.preferences) {
+      if (this.preferences[key]) {
+        this.cookieService.set(key, "true", 365);
+      } else {
+        this.cookieService.delete(key);
+      }
+    }
+
+    this.cookieService.set("cookieConsent", "custom", 365);
+    this.hideCanvasCookies();
+  }
+
+  handlePreferenceChange(type: string): void {
+    this.preferences = {
+      ...this.preferences,
+      [type]: !this.preferences[type]
+    }
+  }
 
   ngAfterViewInit(): void {
-    const canvasElement: HTMLElement = <HTMLElement>document.querySelector(".offcanvas");
-    const buttonClose: HTMLElement = <HTMLElement>document.querySelector(".btn-close");
 
     setTimeout(() => {
-      canvasElement.classList.add("hiding");
-      canvasElement.classList.remove("hiding");
-      canvasElement.classList.add("show");
+      this.showCanvasCookies();
     }, 3000);
 
-    buttonClose.addEventListener("click", () => {
-      canvasElement.classList.remove("show");
-      canvasElement.classList.add("hiding");
-
-      setTimeout(() => {
-        canvasElement.classList.remove("hiding");
-      }, 500);
+    this.buttonCloseCookies.addEventListener("click", () => {
+      this.hideCanvasCookies();
     });
+  }
+
+  showCanvasCookies(): void {
+    this.canvasElement.classList.add("hiding");
+    this.canvasElement.classList.remove("hiding");
+    this.canvasElement.classList.add("show");
+  }
+
+  hideCanvasCookies(): void {
+    this.canvasElement.classList.remove("show");
+    this.canvasElement.classList.add("hiding");
+
+    setTimeout(() => {
+      this.canvasElement.classList.remove("hiding");
+    }, 3000);
+  }
+
+  showModalSettings(): void {
+
   }
 }
